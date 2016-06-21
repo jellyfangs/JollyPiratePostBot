3d6197a98540ce5b4fad7452066ca23f02a4bc6d
jw84 <jerrywang@me.com>
2016-06-21 16:03:11 -0700

    heroku stuff

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ var connector = new builder.ChatConnector({
 // Setup Restify Server
 var server = restify.createServer();
 server.get('/', function (req, res) {
-	res.reply('hello i am a chatbot')
+	res.send('hello i am a chatbot')
 });
 server.post('/api/messages', connector.verifyBotFramework(), connector.listen());
 server.listen(process.env.PORT || 3978, function () {
