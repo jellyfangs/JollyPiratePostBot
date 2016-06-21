7f70db269a67cc14201c51e255222b074a30e6f6
jw84 <jerrywang@me.com>
2016-06-21 16:01:23 -0700

    hello world

--- index.js
+++ index.js
@@ -40,6 +40,9 @@ var connector = new builder.ChatConnector({
 
 // Setup Restify Server
 var server = restify.createServer();
+server.get('/', function (req, res) {
+	res.reply('hello i am a chatbot')
+});
 server.post('/api/messages', connector.verifyBotFramework(), connector.listen());
 server.listen(process.env.port || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
