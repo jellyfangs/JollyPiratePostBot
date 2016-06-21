f544a5dab3b1d3521fb8e47736bb0fd4072fe077
jw84 <jerrywang@me.com>
2016-06-21 16:02:14 -0700

    more heroku heroku stuff

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ server.get('/', function (req, res) {
 	res.reply('hello i am a chatbot')
 });
 server.post('/api/messages', connector.verifyBotFramework(), connector.listen());
-server.listen(process.env.port || 3978, function () {
+server.listen(process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
 
